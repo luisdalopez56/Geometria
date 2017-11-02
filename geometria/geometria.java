7bb8d91e4e1c15365f929534b5a772ff8dee5454
Manuel de la Torre <[mtorrecano@gmail.com]>
2017-11-02 14:16:51 +0100

    Commit 1º de desarrollo
    Creacion de documentacion

--- geometria/geometria.java
+++ geometria/geometria.java
@@ -1,7 +1,32 @@
+/**Programa que nos ayuda 
+a realizar diversas operaciones
+@author Luisda
+@version 1.0
+*/
+
 public class geometria{
+
+/**Metodo CalculaAreaCuadrado
+nos calcula el area de un
+cuadrado.
+@param lado que el lado que define al cuadrado
+@return area del cuadrado, de tipo double
+*/
+
+
 	public static double calcularAreaCuadrado (double lado){
 		return lado*lado;
 	}
+
+/**Metodo CalculaAreaRectangulo
+Calcula el area de un
+rectangulo conociendo
+su altura y su base.
+@param base del tipo double,base del rectangulo
+@param altura del tipo double, altura del rectangulo
+@return area del rectangulo, de tipo double
+*/
+
 	public static double calcularAreaRectangulo (double base, double altura){
                 return base*altura;
 	
