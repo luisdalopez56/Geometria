86b0289503e6ff65920beeb74462539bb671a988
Manuel de la Torre <[mtorrecano@gmail.com]>
2017-11-02 13:33:06 +0100

    Añadido carpeta de Geometria

---
+++ geometria/geometria.java
@@ -0,0 +1,9 @@
+public class geometria{
+	public static double calcularAreaCuadrado (double lado){
+		return lado*lado;
+	}
+	public static double calcularAreaRectangulo (double ladorect,base); {
+                return ladorect*base/2;
+	
+	}
+}
---
+++ geometria/testgeometria.java
@@ -0,0 +1,13 @@
+public class testgeometria{
+
+	public static void main(String[]args){
+	
+	double areaCuadrado = geometria.calcularAreaCuadrado(2.123);
+	System.out.println("Area del cuadrado de lado 2.123 vale: " + areaCuadrado);
+	
+	double areaRectangulo = geometria.calcularAreaRectangulo(2.123,1.233);
+        System.out.println("Area del rectangulo de base 2.123 y altura 1.233 vale: " + areaRectangulo);	
+
+	}
+}
+
